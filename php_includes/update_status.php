c5b8950b635c925b49cedf63ea13ad93e4e87e3a
Danish <dani3250@gmail.com>
2015-01-06 00:09:22 +0530

    Revert "This is the first alpha version"
    This reverts commit e8ccd17bc5beed9bbbfe3389501dd4d0f62721f9.

--- php_includes/update_status.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-
-$status = $_GET['status'];
-$id = $_GET['id'];
-mysql_query("UPDATE tbl_mock SET fk_status='$status' WHERE id=$id");
-echo "Record updated successfully";
-?>
